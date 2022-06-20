b035b994e958a4f85e9502ea8d43eb61f26ac9ee
Yash Desai <yddc902@live.com>
2022-06-20 08:26:10 +0530

    signin route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const { ENV } = require("./utils");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", au);
+
+const PORT = ENV.PORT;
+app.listen(PORT, async () => {
+  try {
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
